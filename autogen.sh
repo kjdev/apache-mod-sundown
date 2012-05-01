8f9d2b63019ede166e8b73c8d1bb56cf9f7d608c
Tatsuya Kamijo <kjclev@gmail.com>
2012-05-01 21:20:10 +0900

    Updated: autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,14 @@ set -e
 
 # Constants
 ACLOCAL="aclocal"
+AUTOHEADER="autoheader"
 AUTOMAKE="automake"
 AUTOCONF="autoconf"
 LIBTOOLIZE="libtoolize"
 
 # Cleanup
 echo "Cleanup"
-rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h
+rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h config.h.in
 rm -f *.o *.la *.lo *.slo Makefile.in Makefile
 
 if [ "$1" = "clean" ]; then
@@ -22,6 +23,9 @@ ${ACLOCAL}
 
 echo "Running libtoolize"
 ${LIBTOOLIZE} -c
+
+echo "Running autoheader"
+${AUTOHEADER}
 
 echo "Running automake"
 ${AUTOMAKE} -a
