cdf22acd139109cf6bcdef180d4bc838ae1fca42
kjdev <kjclev@gmail.com>
2013-02-21 10:48:07 +0900

    Deleted RPM packages. Updated configure.ac, autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,7 @@ LIBTOOLIZE="libtoolize"
 
 # Cleanup
 echo "Cleanup"
-rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h config.h.in m4
+rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h config.h.in m4 ar-lib
 rm -f *.o *.la *.lo *.slo Makefile.in Makefile
 
 if [ "$1" = "clean" ]; then
